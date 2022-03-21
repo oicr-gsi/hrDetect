143e60a8b9e6ab27139c8f12a89e65a7c561eee1
Felix Beaudry <felix.beaudry@gmail.com>
2022-03-21 13:11:45 -0400

    Update sigTools_runthrough.R

--- sigTools_runthrough.R
+++ sigTools_runthrough.R
@@ -91,7 +91,10 @@ write.table(
     "HRD_point"=HRDetect_res$hrdetect_output[8],
     quantiles
   ),
-  file = paste(wkdir,sample_name,".sigtools.hrd.txt",sep="")
+  file = paste(wkdir,sample_name,".sigtools.hrd.txt",sep=""),
+  append = F, quote = FALSE, sep = "\t", 
+  eol = "\n", na = "NA",dec = ".", row.names = TRUE, 
+  col.names = FALSE
 )
 
 #sigsToUse <- getOrganSignatures(tissue,typemut = "subs") 
