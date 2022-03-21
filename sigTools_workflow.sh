1d28eab7a245e86407b9c371908e52791e4e261b
Felix Beaudry <felix.beaudry@gmail.com>
2022-03-21 11:36:36 -0400

    updates to workflow

--- sigTools_workflow.sh
+++ sigTools_workflow.sh
@@ -1,68 +1,59 @@
-#required files:
-#excludedRegions: https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/genome-stratifications/v3.0/GRCh38/union/ 
-#https://github.com/hall-lab/svtools
-#or illumina's compiled fork; https://github.com/ctsa/svtools 
-#our fork https://github.com/oicr-gsi/svtools/releases/tag/v0.5.2
+
 
 ssh ugehn01.hpc.oicr.on.ca
+qrsh -P gsi -l h_vmem=10G -q u18build
 
-qrsh -P gsi -l h_vmem=10G #-q u18build
-
-module load gatk tabix bcftools #GRCh38-alldifficultregions
-
-module load python/2.7
-export PATH=/scratch2/users/ibancarz/svtools/install_03/bin:$PATH
-export PYTHONPATH=/scratch2/users/ibancarz/svtools/install_03:$PYTHONPATH
 
 wrkdir=/.mounts/labs/CGI/scratch/fbeaudry/sigTools_test/
-studyLocation=scratch/fbeaudry/
+cd $wrkdir
 
 study=TGL62
+studyLocation=/.mounts/labs/CGI/scratch/fbeaudry/
+
+VAF=0.15
+
 HRDtissue=Ovary
 
 #for sampleRoot in OCT_010359
+do
 sampleRoot=OCT_010958
 
-sample_pt1=$(tail -n +2 /.mounts/labs/CGI/${studyLocation}/${study}/${sampleRoot}/report/data_clinical.txt | awk '{print $3}')
+module load grch38-alldifficultregions hg38/p12
 
-
-reference=/.mounts/labs/gsi/modulator/sw/data/hg38-p12/hg38_random.fa
-
-excludedRegions=GRCh38_alldifficultregions.bed
-		env | grep 'exclusion' 
-
-cd ${wrkdir}
+#reference=/.mounts/labs/gsi/modulator/sw/data/hg38-p12/hg38_random.fa
+#GRCH38_ALLDIFFICULTREGIONS_ROOT=/.mounts/labs/gsi/modulator/sw/data/grch38-alldifficultregions-3.0/GRCh38_alldifficultregions.bed
 
 SNV_file=$(zgrep ${study} $PROVREP | awk -v study="$study" -F "\t" '$2 == study' | grep ${sampleRoot} | grep filter.deduped.realigned.recalibrated.mutect2.vcf.gz | cut -f1,2,14,31,47 | sort -r  | uniq | awk '$6 !~ ".tbi" {print $6}' |  head -n 1)
-SV_file=$(zgrep ${study} $PROVREP | awk -v study="$study" -F "\t" '$2 == study' |  grep ${sampleRoot} | grep delly.merged.vcf.gz  | cut -f1,2,14,31,47 | sort -r  | uniq | awk '$6 !~ ".tbi" {print $6}' |  head -n 1 )
+SV_file=$(zgrep ${study} $PROVREP | awk -v study="$study" -F "\t" '$2 == study' |  grep ${sampleRoot} | grep filtered.delly.merged.vcf.gz  | cut -f1,2,14,31,47 | sort -r  | uniq | awk '$6 !~ ".tbi" {print $6}' |  head -n 1 )
+
+module load gatk tabix bcftools  
 
 ##task No1 make .bedpe
 
-svtools vcftobedpe -i ${SV_file} -o ${sampleRoot}.somatic.delly.merged.bedpe
-
 echo  -e "chrom1\tstart1\tend1\tchrom2\tstart2\tend2\tsample\tsvclass"  >${sampleRoot}.somatic.delly.merged.reformat.bedpe
-awk -v sample_t=${sample_pt1}_WG${sample_pt2} '$12 ~ "PASS" {print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"sample_t"\t"$11}' ${sampleRoot}.somatic.delly.merged.bedpe >>${sampleRoot}.somatic.delly.merged.reformat.bedpe
+
+bcftools query -f "%CHROM\t%POS\t%INFO/END\t%FILTER\t%ALT\t%INFO/CIPOS\t%INFO/CIEND\t[%DR\t]\t[%DV\t]\t[%RR\t]\t[%RV\t]\n" ${SV_file} | awk '$5 !~ ":" {print}' | awk '$4 ~ "PASS" {print}' | awk -v VAF=$VAF '($10+$14)/($8+$10+$12+$14) > VAF {print}' | awk -v sample_t=${sampleRoot}  'split($6,a,",") split($7,b,",") {print $1"\t"$2+a[1]-1"\t"$2+a[2]"\t"$1"\t"$3+b[1]-1"\t"$2+b[2]"\t"sample_t"\t"$5} ' | sed 's/<//g; s/>//g'  >>${sampleRoot}.somatic.delly.merged.reformat.bedpe
 
 ##task No2 make seperate .vcf files for SNVs and indels 
-bcftools filter -i '(FORMAT/AD[0:1]*100)/(FORMAT/AD[0:0]+FORMAT/AD[0:1]) >= 15' ${SNV_file} >${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.vcf
+
+bcftools filter -i "(FORMAT/AD[0:1])/(FORMAT/AD[0:0]+FORMAT/AD[0:1]) >= ${VAF}" ${SNV_file} >${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.vcf
 
 for varType in SNP INDEL
 do
 
-gatk SelectVariants -R ${reference}  --exclude-intervals ${excludedRegions} -V ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.vcf  --select-type-to-include ${varType} -O ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.${varType}.vcf
+	gatk SelectVariants -R ${HG38_ROOT}/hg38_random.fa  --exclude-intervals ${GRCH38_ALLDIFFICULTREGIONS_ROOT}/GRCh38_alldifficultregions.bed -V ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.vcf  --select-type-to-include ${varType} -O ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.${varType}.vcf
 
-bgzip ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.${varType}.vcf
+	bgzip ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.${varType}.vcf
 
-tabix -p vcf ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.${varType}.vcf.gz
+	tabix -p vcf ${sampleRoot}.filter.deduped.realigned.recalibrated.mutect2.MAF15.${varType}.vcf.gz
 done
 
 ##task No3 Copy number for loss-of-heterozygosity
-echo  -e "seg_no\tChromosome\tchromStart\tchromEnd\ttotal.copy.number.inNormal\tminor.copy.number.inNormal\ttotal.copy.number.inTumour\tminor.copy.number.inTumour"  >${wrkdir}/${sampleRoot}_segments.cna.txt
 
-sequenza_explorer.py locate --donor ${sampleRoot} --project ${study} >sequenza_loc.tmp
-sequenza_loc=$(cat sequenza_loc.tmp)
-cp ${sequenza_loc} ./
-unzip ${sampleRoot}_*_results.zip
+#sequenza_explorer.py locate --donor ${sampleRoot} --project ${study} >sequenza_loc.tmp
+#sequenza_loc=$(cat sequenza_loc.tmp)
+#cp ${sequenza_loc} ./
+#unzip ${sampleRoot}_*_results.zip
 
 gamma=$(awk 'split($1,a,"=") $1 ~ "sequenza_gamma" {print a[2]}'  /.mounts/labs/CGI/${studyLocation}/${study}/${sampleRoot}/config.ini )
 
@@ -71,7 +62,8 @@ echo  -e "seg_no\tChromosome\tchromStart\tchromEnd\ttotal.copy.number.inNormal\t
 tail -n +2 /.mounts/labs/CGI/${studyLocation}/${study}/${sampleRoot}/gammas/${gamma}/${sampleRoot}*_segments.txt | awk 'split($1,a,"\"") split(a[2],b,"chr") {print NR"\t"b[2]"\t"$2"\t"$3"\t"2"\t"1"\t"$10"\t"$12}'  >>${wrkdir}/${sampleRoot}_segments.cna.txt
 
 ##task No4 run everything through the sigTools script
+
 module load sigtools
 
 Rscript --vanilla sigTools_runthrough.R ${sampleRoot} ${HRDtissue} ${wrkdir}
-
+done
