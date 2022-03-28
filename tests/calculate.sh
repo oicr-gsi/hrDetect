2d1a67359be20bd3bcb1b623fb006b5d2ae7c749
Felix Beaudry <felix.beaudry@gmail.com>
2022-03-28 16:48:25 -0400

    Update calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,3 +6,5 @@ set -o pipefail
 cd $1
 
 find . -regex '.*\.sigtools.hrd.txt$' -exec md5sum {} \;
+find . -regex '.*\.sigtools.model.txt$' -exec md5sum {} \;
+find . -regex '.*\.sigtools.sigs.txt$' -exec md5sum {} \;
