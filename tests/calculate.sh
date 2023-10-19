80b696d145e614c3ceefdafb5b066650c48df0ac
Gavin Peng <gpeng@oicr.on.ca>
2023-10-19 16:05:20 -0400

    Won't test md5sum for non-determinstic outputs

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o pipefail
 
 cd $1
-find . -name '*.json' -xtype f -exec sh -c "cat {} | md5sum" \;
+find . -regex '.*\.SBS.json$' -exec md5sum {} \;
+find . -regex '.*\.ID.json$' -exec md5sum {} \;
 ls | sed 's/.*\.//' | sort | uniq -c
